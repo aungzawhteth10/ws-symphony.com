cafaeb93927f6501a188f62da49a51e9af39b59c
AungZawHtet <aungzawhteth10@gmail.com>
2021-07-10 12:18:50 +0900

    deploy test

--- app/ui/routes_ui.php
+++ app/ui/routes_ui.php
@@ -3,6 +3,5 @@ $app->get('/', function ($request, $response) {
     return $this->view->render($response, 'index.twig', []);
 });
 $app->get('/{id}', function ($request, $response, $args) {
-    return $args['id'];
     return $this->view->render($response, $args['id'] . '.twig', []);
 });
