883acfd09570cadcd693ce275e7416124bf9a195
AungZawHtet <aungzawhteth10@gmail.com>
2021-07-10 11:58:45 +0900

    deploy test

--- app/ui/routes_ui.php
+++ app/ui/routes_ui.php
@@ -1,11 +1,8 @@
 <?php
 $app->get('/', function ($request, $response) {
-    return 'ws-symphony';
     return $this->view->render($response, 'index.twig', []);
 });
-$app->get('ws-symphony.com/aaa', function ($request, $response, $args) {
-    return 'get AAA';
+$app->get('/{id}', function ($request, $response, $args) {
+    return $args['id'];
+    return $this->view->render($response, $args['id'] . '.twig', []);
 });
-// $app->get('/{id}', function ($request, $response, $args) {
-//     return $this->view->render($response, $args['id'] . '.twig', []);
-// });
