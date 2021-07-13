dff1ba80945f790b50d10e2a329f39a62f51d14d
AungZawHtet <aungzawhteth10@gmail.com>
2021-07-13 22:44:28 +0900

    routes_ui test

--- src/info4u/app/ui/routes_ui.php
+++ src/info4u/app/ui/routes_ui.php
@@ -3,9 +3,13 @@ $app->get('/info4u/', function ($request, $response) {
     return $this->viewInfo4u->render($response, 'index.twig', []);
 });
 $app->get('/info4u', function ($request, $response) {
-    return '<script>location.href= "/info4u/"</script>';
+    return redirect('/info4u/');
 });
-
 $app->get('/info4u/{id}', function ($request, $response, $args) {
     return $this->viewInfo4u->render($response, $args['id'] . '.twig', []);
 });
+
+function redirect ($url) 
+{
+    return '<script>location.href= "'. $url . '"</script>';
+}
